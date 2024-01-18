91bf09524b70754082ae625259f5635d20cdeda0
BetterMInt <bettermint@basicsellout.tk>
2024-01-18 05:14:34 -0500

    Update relax.cpp

--- bettermint/features/relax.cpp
+++ bettermint/features/relax.cpp
@@ -15,17 +15,19 @@ bool debug_relax = false;
 
 static char current_click = cfg_relax_style == 'a' ? right_click[0] : left_click[0];
 
+float rand_range_f(float f_min, float f_max)
+{
+    float scale = rand() / (float)RAND_MAX;
+    return f_min + scale * (f_max - f_min);
+}
+
+int rand_range_i(int i_min, int i_max)
+{
+    return rand() % (i_max + 1 - i_min) + i_min;
+}
+
 void calc_od_timing()
 {
-    static const auto rand_range_f = [](float f_min, float f_max) -> float
-    {
-        float scale = rand() / (float)RAND_MAX;
-        return f_min + scale * (f_max - f_min);
-    };
-    static const auto rand_range_i = [](int i_min, int i_max) -> int
-    {
-        return rand() % (i_max + 1 - i_min) + i_min;
-    };
     if (cfg_relax_checks_od && (od_check_ms == .0f))
     {
         od_check_ms = rand_range_f(od_window_left_offset, od_window_right_offset);
